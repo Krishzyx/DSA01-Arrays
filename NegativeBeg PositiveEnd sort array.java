54c38f6c3171fa357e5ce8b5f76e44c0ca6e09cc
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2023-07-05 19:26:43 +0530

    Create NegativeBeg PositiveEnd sort array

---
+++ NegativeBeg PositiveEnd sort array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solutions {
+    public static void move(int[] arr)
+    {
+        Arrays.sort(arr);
+    }
+
+//Driver code
+public static void main (String [] args)
+{
+    int arr[] = {-1,2,-3,4,5,6,-7,8,9};
+    move(arr);
+    for(int e: arr)
+    System.out.print(e + " ");
+}
+
+}
