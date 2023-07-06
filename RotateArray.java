7be3d38ba2bf40a04f69973a26646c5f62d91237
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2023-07-06 08:47:50 +0530

    Create RotateArray

---
+++ RotateArray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+ class Test
+{
+    static int  arr[] = new int []{1,2,3,4,5};
+
+    //method for Rotation 
+    static void rotate()
+    {
+        int x = arr[arr.length-1],i;
+        for (i=arr.length-1;i>0;i--);
+        arr[i]=arr[i-1];
+        arr[0] = x;
+
+    }
+   public static void main (String []args)
+   {
+    System.out.println("Given Array is");
+    System.out.println(Arrays.toString(arr));
+
+    rotate();
+ 
+    System.out.println("Rotated Array is");
+    System.out.println(Arrays.toString(arr));
+   }
+
+}
