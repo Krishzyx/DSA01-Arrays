01feee17d3fd2bcfdc26a7a151270df4207a2407
Prince Maurya <121998960+Krishzyx@users.noreply.github.com>
2023-07-07 19:40:24 +0530

    Create SearchInRotatedarray

---
+++ SearchInRotatedarray.java
@@ -0,0 +1,41 @@
+class Solution {
+    static int search (int arr[], int l,int h, int key)
+{
+    if (l>h)
+    return -1;
+ 
+    int  mid = (l+h)/2;
+    if( arr[mid]== key)
+    return mid;
+
+    if (arr[l] <= arr[mid])
+    {
+        if( key >= arr[l] && key <= arr[mid])
+        return search (arr,mid-1,h,key);
+
+        return search(arr, l,mid + 1, key);
+
+    }
+    if( key >= arr[l] && key <= arr[mid])
+        return search (arr,mid+1,h,key);
+
+        return search(arr, l,mid - 1, key);
+
+ 
+} 
+
+//main function 
+public static void main (String args [])
+{
+    int arr[] = { 4,5,6,7,8,9,1,2,3};
+    int n = arr.length;
+    int key = 3;
+    int i = search(arr,0,n-1,key);
+    if ( i!= -1)
+    System.out.println("Index: "  + i);
+    else 
+    System.out.println("Key not found ");
+
+}
+
+}
